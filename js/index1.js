013082b559d9960392a185f4616f602531dc1b3b
Audrey BONNET <audrey.bonnet.45@outlook.com>
2020-04-03 10:48:21 +0200

    modif page 1 et index 1

---
+++ js/index1.js
@@ -0,0 +1,20 @@
+const bpSend = document.getElementById("bpValid");
+
+
+function addElement () {
+  // crée un nouvel élément div
+  let newDiv = document.createElement("div");
+  // et lui donne un peu de contenu
+  let newContent = document.createTextNode('Hi there and greetings!');
+  // ajoute le nœud texte au nouveau div créé
+  newDiv.appendChild(newContent);
+  
+  // ajoute le nouvel élément créé et son contenu dans le DOM
+  let currentDiv = document.getElementById('div1');
+  document.getElementById("notesBoard").insertBefore(newDiv, currentDiv);
+
+}
+
+
+bpSend.addEventListener("click", addElement);
+document.getElementById("notesBoard").onload = addElement;
